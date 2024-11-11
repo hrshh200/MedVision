89fad1cd2fb02fab04585786f84591f42db8b463
Harsh <hsharmarishra@gmail.com>
2024-11-11 12:01:27 +0530

    Updated SearchDoctor

--- frontend/src/pages/SearchDoctor.jsx
+++ frontend/src/pages/SearchDoctor.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const SearchDoctor = () => {
     return (
-       <div>
-            <h1 className="text-blue-600">Search Doctor History</h1>
+       <div className='mt-[10vh]'> 
+            <h1 className="text-blue-600 z-[2000] ">Search Doctor History</h1>
        </div>
     )
 }
 
-export default SearchDoctor;
+export default SearchDoctor;    
