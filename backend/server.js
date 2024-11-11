7c703bed843ce63d0cb83e44d28b7ed965300de1
Harsh <hsharmarishra@gmail.com>
2024-11-11 11:35:07 +0530

    Changed port

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const start = async () => {
         else {
             console.log("Error connecting to database");
         }
-        app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+        app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
             console.log(`Server is running on port ${process.env.PORT || 3000}`);
           });
     } catch (error) {
