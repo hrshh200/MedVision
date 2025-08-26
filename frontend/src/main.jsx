22e86ddb32d4a2f40eb31e7788cf684f98f9f7d6
hrshh200 <hsharmarishra@gmail.com>
2025-08-26 14:12:44 +0530

    Changed baseURL to render deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,8 @@ import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import './index.css'
 
-// export const baseURL = 'https://medvision-szb6.onrender.com/api';
-export const baseURL = 'http://localhost:5001/api';
+export const baseURL = 'https://medvision-szb6.onrender.com/api';
+// export const baseURL = 'http://localhost:5001/api';
 
 
 createRoot(document.getElementById('root')).render(
